705b4caa79b64ca0e3887271753a0d2576ccca95
cherbourg@fub.fr <cherbourg@fub.fr>
2023-05-10 20:25:31 +0200

    Add MealPlanner

--- src/components/MealPlanner.vue
+++ src/components/MealPlanner.vue
@@ -1,6 +1,9 @@
 <script setup>
 import { ref } from 'vue';
 import draggable from 'vuedraggable';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
+
 const meals = ref([
   'Hamburger',
   'Pizza',
@@ -21,20 +24,50 @@ const yuckyMeals = ref([
   'Anything from Taco Bell',
 ]);
 
-const columns = ref([
-  { header: "Id", name:"id"},
-  { header: "Name", name:"name" },
-  { header: "Status", name:"status", displayField: "isActive" },
-  { header: "Action", name:"action" },
-]);
+const exportPDF = () => {
+  const doc = new jsPDF();
+  const date = date();
+  const fileName = "Planning"+date+".pdf";
+  doc.autoTable({
+    head: [['Nom', 'Âge', 'Email']],
+    body: items.map(item => [item.name, item.age, item.email])
+  });
+  doc.save("Planning.pdf");
+};
+
+const items = [
+  { id: 1, name: 'John Doe', age: 30, email: 'john.doe@example.com' },
+  { id: 2, name: 'Jane Doe', age: 25, email: 'jane.doe@example.com' },
+  { id: 3, name: 'Bob Smith', age: 40, email: 'bob.smith@example.com' }
+];
 
 </script>
 
 
 
 <template>
+  <h1>Meal Planner</h1>
+  <button @click="exportPDF" >Export to PDF</button>
+      <table>
+      <thead>
+        <tr>
+          <th>Nom</th>
+          <th>Âge</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in items" :key="item.id">
+          <td>{{ item.name }}</td>
+          <td>{{ item.age }}</td>
+          <td>{{ item.email }}</td>
+        </tr>
+      </tbody>
+    </table>
+<div class="dashboard">
 
-<table>
+
+<table >
       <tr>
         <th>Day</th>
         <th>Lunch</th>
@@ -61,21 +94,37 @@ const columns = ref([
         <td></td>
         <td></td>
       </tr>
+      <tr>
+        <td>Wednesday</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Thursday</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Friday</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Saturday</td>
+        <td></td>
+        <td></td>
+      </tr>
+      <tr>
+        <td>Sunday</td>
+        <td></td>
+        <td></td>
+      </tr>
     </table>
+    
+
+<div class="element-to-convert">
+  
     hello,world!
-  <h1>Lundi</h1>
-  <h1>Lundi lunch</h1>
-  <draggable v-model="mondayLunch" tag="ul" group="meals">
-    <template #item="{ element: meal }">
-      <li>{{ meal }}</li>
-    </template>
-  </draggable>
-  <h1>Lundi dinner</h1>
-  <draggable v-model="mondayDinner" tag="ul" group="meals">
-    <template #item="{ element: meal }">
-      <li>{{ meal }}</li>
-    </template>
-  </draggable>
   <h1>Favorite Foods</h1>
 
   <draggable v-model="meals" tag="ul" group="meals">
@@ -90,9 +139,21 @@ const columns = ref([
       <li>{{ meal }}</li>
     </template>
   </draggable>
+  
+</div>
+</div>
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Darker+Grotesque:wght@900&display=swap');
+
+h1 {
+    font-size: 25px;
+    font-family: 'Darker Grotesque', sans-serif;
+    color: #02BD7E;
+    text-align: center;
+}
+
 
 li {
     list-style: none;
@@ -104,7 +165,7 @@ li {
 }
 
 table{ 
-    width: 400px;
+    width: 800px;
     height: 150px;
     margin: auto;
     text-align: center;
@@ -119,10 +180,21 @@ tr
 
 }
 
-td {
+td,th  {
     width: 150px;
     height: 80px;
     border: solid 1px #02BD7E ;
+
+}
+
+.dashboard {
+  display: flex;
+  align-items: center;
+}
+
+.element-to-convert {
+  display: flex;
+  flex-direction: column;
 
 }
 
---
+++ src/views/RecipieDetailView.vue
@@ -0,0 +1,10 @@
+<template>
+    Hello {{ $route.params.id }}, world!
+</template>
+
+<script setup>
+import {ref, onMounted} from "vue";
+import {useRoute} 
+
+</script>
+https://www.youtube.com/watch?v=sWaBtQBXY2o&ab_channel=CodeWithTony
