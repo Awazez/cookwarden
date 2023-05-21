925f14d8af3ade41412a84ff718827835821429d
cherbourg@fub.fr <cherbourg@fub.fr>
2023-05-21 23:13:14 +0200

    update

--- src/views/RecipieDetailView.vue
+++ src/views/RecipieDetailView.vue
@@ -4,7 +4,18 @@
 
 <script setup>
 import {ref, onMounted} from "vue";
-import {useRoute} 
+import {useRoute} from 'vue-router';
+
+const queryMovie = ref({})
+const isLoading = ref(true)
+const route = useRoute();
+
+onMounted(async() => {
+    const result = await fetch("http://localhost:8000/movies${route.params.id}");
+    const response = result.json();
+    queryMovie.value = response;
+    isLoading.value = false;
+})
 
 </script>
 https://www.youtube.com/watch?v=sWaBtQBXY2o&ab_channel=CodeWithTony
