6dd72056bbf8120693e9afdbad4256b610fce297
cherbourg@fub.fr <cherbourg@fub.fr>
2023-05-21 23:16:05 +0200

    del problems

--- src/views/RecipieDetailView.vue
+++ src/views/RecipieDetailView.vue
@@ -3,19 +3,5 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from "vue";
-import {useRoute} from 'vue-router';
-
-const queryMovie = ref({})
-const isLoading = ref(true)
-const route = useRoute();
-
-onMounted(async() => {
-    const result = await fetch("http://localhost:8000/movies${route.params.id}");
-    const response = result.json();
-    queryMovie.value = response;
-    isLoading.value = false;
-})
-
 </script>
 https://www.youtube.com/watch?v=sWaBtQBXY2o&ab_channel=CodeWithTony
